d2639ded6994f1bd24036329c6087fd9e51304e8
Tamir Nakar <nakar.tamir@gmail.com>
2020-07-24 19:16:37 +0300

    minimize popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,7 @@
 import AppManager from "./appManager.js";
 //data layer
 let appManager;
-let groupsMap = {};
-let displayNameSet = new Set();
-//dom elements
-let aliases;
-let groupsSideBar;
-//etc
-let groupContextMenu;
-let itemContextMenu;
-let contextMenuTarget;
-let menus;
+
 
 async function _init() {
 
@@ -22,63 +13,6 @@ async function _init() {
 window.addEventListener("DOMContentLoaded", async () => {
     await _init();
 });
-
-const swalAlert = Swal.mixin({
-  customClass: {
-    confirmButton: "button success",
-    cancelButton: "button danger",
-  },
-  buttonsStyling: false,
-});
-
-function remove(alias) {
-  swalAlert
-    .fire({
-      title: "Are you sure?",
-      text: `Delete '${alias}' ?`,
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Yes, delete it!",
-      cancelButtonText: "No, cancel!",
-      reverseButtons: true,
-    })
-    .then((result) => {
-      if (result.value) {
-        chrome.storage.sync.remove(alias, function () {
-          $(`[id='${alias}']`).remove();
-        });
-        Swal.fire("Success!", `'${alias}' was deleted successfully`, "success");
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-      }
-    });
-}
-function _removeItemFromGroupsMap(group, displayName) {
-  const index = groupsMap[group].findIndex(
-    (item) => item.displayName === displayName
-  );
-  if (index > -1) {
-    groupsMap[group].splice(index, 1);
-  }
-}
-function _storeDataAsync(data) { // todo done
-  return new Promise((resolve, reject) => {
-    chrome.storage.sync.set(data, () => {
-      if (chrome.runtime.lastError) {
-        reject();
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-function _initDomReferences() {
-  aliases = document.querySelector("#aliases");
-  groupsSideBar = document.querySelector("#rightSideBar");
-}
 
 function _initEvents() {
   document
@@ -106,12 +40,4 @@ function _initEvents() {
     event.preventDefault();
   });
 }
-
-function _clearActiveGroupItems() {
-  //const activeGroupName = _getActiveGroupName();
-
-
-}
-
-//clear();
 

5c1fa95aa4daf84ee73f97685681d84990e29c4e
Tamir Nakar <nakar.tamir@gmail.com>
2020-08-29 00:43:14 +0300

    1. bugfix: aliases survive on browser restart 2. no 'unsef-eval' 3. no jquery 4. background.js is a module now (background.html created)

--- js/background.js
+++ js/background.js
@@ -1,51 +1,59 @@
-let aliasesMap = {};
+import StorageManager from './storageManager.js';
 
-function _updateAliasesMap() {
-    aliasesMap = {};
-    chrome.storage.sync.get(null, function (data) {
-        if (data) {
-
-            for(let item in data){
-                // handle alias
-                const itemAlias = data[item].alias;
-                if (itemAlias && itemAlias !== "_dummy_") {
-                    aliasesMap[itemAlias] = data[item].url;
-                }
-            }
-        }
-    });
-}
-
-
-chrome.storage.onChanged.addListener( () =>  _updateAliasesMap());
-
-const re = /[\d\s\+\-=\(\)\*]+/g;
+const aliasesMap = {};
 const link = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
 
-// On input changed, call this
-chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-    const suggestions = [];
-    for (key in aliasesMap) {
-        if (key.startsWith(text) || text == "") {
-            var desc = `<match>${text}: </match><dim>${key} → </dim><url>${aliasesMap[key]}</url>`;
-            suggestions.push({content: aliasesMap[key], description: desc});
+async function _updateAliasesMap() {
+
+    const data = await StorageManager.getAllItemsAsync();
+
+    if (data) {
+        for(let item in data) {
+            // handle alias
+            const itemAlias = data[item].alias;
+
+            if (itemAlias && itemAlias !== "_dummy_") {
+
+                aliasesMap[itemAlias] = data[item].url;
+            }
         }
     }
-    if (text.match(re)) {
-        var result = eval(text).toString();
-        // alert(result);
-        chrome.omnibox.setDefaultSuggestion({
-            description: `<match>= </match><url>${result}</url>`,
-        });
-    } else if (suggestions.length > 0) {
-        var first = suggestions.splice(0, 1)[0];
-        chrome.omnibox.setDefaultSuggestion({description: first["description"]});
-    } else {
-        chrome.omnibox.setDefaultSuggestion({
-            description: `<match>${text}: </match><dim> - Google Search</dim>`,
-        });
+}
+
+(async () => await _updateAliasesMap())(); // when browser up -> update aliases map
+chrome.storage.onChanged.addListener( () =>  _updateAliasesMap()); // on every storage change -> update aliases map
+
+// called on every key stroke in the omnibox
+chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+
+    const suggestions = [];
+
+    for (let key in aliasesMap) { // prepare all suggestions
+
+        if (text && key.startsWith(text)) {
+
+            const url = aliasesMap[key];
+            const description = `<match>${text}: </match><dim>${key}</dim>`;
+            const suggestResult = {content: url, description};
+            suggestions.push(suggestResult);
+        }
     }
-    suggest(suggestions);
+
+        if (suggestions.length > 0) {
+        const first = suggestions.splice(0, 1)[0];
+        chrome.omnibox.setDefaultSuggestion({description: first["description"]});
+    }
+
+
+    // else {
+    //     chrome.omnibox.setDefaultSuggestion({
+    //         description: `<match>${text}: </match><dim> - Google Search</dim>`,
+    //     });
+    // }
+    if(suggestions.length) {
+
+        suggest(suggestions);
+    }
 });
 
 // User has accepted what is typed into the omnibox. (by pressing enter)
@@ -54,19 +62,11 @@ chrome.omnibox.onInputEntered.addListener(function (text) {
         chrome.tabs.update({url: aliasesMap[text]});
     } else if (text.match(link)) {
         chrome.tabs.update({url: text});
-    } else if (text.match(re)) {
-        var result = eval(text).toString();
-        chrome.tabs.update({url: `https://google.com/search?q=${result}`});
-    } else {
-        chrome.tabs.update({url: `https://google.com/search?q=${text}`});
     }
-});
-
-// Starting input
-chrome.omnibox.onInputStarted.addListener(function () {
-    chrome.storage.sync.get(null, function (obj) {
-        for (o in obj) {
-            aliasesMap[o] = obj[o];
-        }
-    });
+    // else if (text.match(re)) {
+    //     var result = eval(text).toString();
+    //     chrome.tabs.update({url: `https://google.com/search?q=${result}`});
+    // } else {
+    //     chrome.tabs.update({url: `https://google.com/search?q=${text}`});
+    // }
 });
